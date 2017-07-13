22f261533317c2d8bf431b4e932204cf8e646bb1
jarjin <jarjin@163.com>
2017-07-13 21:24:48 +0800

    更新至tolua 1.0.7.334 fixed LuaTable this 操作问题

--- Assets/LuaFramework/ToLua/Core/LuaDLL.cs
+++ Assets/LuaFramework/ToLua/Core/LuaDLL.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (c) 2015-2016 topameng(topameng@qq.com)
+Copyright (c) 2015-2017 topameng(topameng@qq.com)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -187,7 +187,7 @@ namespace LuaInterface
 
     public class LuaDLL
     {
-        public static string version = "1.0.7.326";
+        public static string version = "1.0.7.335";
         public static int LUA_MULTRET = -1;
         public static string[] LuaTypeName = { "none", "nil", "boolean", "lightuserdata", "number", "string", "table", "function", "userdata", "thread" };        
 
